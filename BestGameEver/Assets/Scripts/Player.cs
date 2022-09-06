0c60412529dffb1aaaac1b127939cfcb6382eccf
Angie <36361212+LapisRaider@users.noreply.github.com>
2022-09-07 02:10:32 +0800

    Charmandar updated Player

--- BestGameEver/Assets/Scripts/Player.cs
+++ BestGameEver/Assets/Scripts/Player.cs
@@ -1,17 +1,23 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private int charmandarSaysHi = 10;
+
     void NewFunction()
     {
-
+        charmandarSaysHi = 21;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int charmandarVariable = 10;
+    }
+
+    void charmandarAddedThis()
+    {
+
     }
 }
